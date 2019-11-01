6fe417617845d2a7bea1a3eef6cc1d9154cedbab
Johnathan Bressette <bressetteprogramming@gmail.com>
2019-11-01 08:27:50 -0400

    Add insertHeap to insert element to heap

--- main.c
+++ main.c
@@ -8,7 +8,38 @@ struct minHeap
     int numElements;
 };
 
+struct minHeap insertHeap(struct minHeap heap, int data)
+{
+    if(heap.heapSize == heap.numElements)
+    {
+        printf("The heap is full\n");
+        return heap;
+    }
+
+    else
+    {
+        int index = heap.numElements;
+        heap.arr[index] = data;
+        int parentIndex = (index-1)/2;
+        heap.numElements++;
+
+        while(index != 0) && heap.arr[parentIndex] > heap.arr[index])
+        {
+            int temp = heap.arr[index];
+            heap.arr[index] = heap.arr[parentIndex];
+            heap.arr[parentIndex] = temp;
+            index = parentIndex;
+            parentIndex = (index-1)/2;
+        }
+        return heap;
+    }
+
+}
+
+
+
 int main()
 {
 
+    return 0;
 }
