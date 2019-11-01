e22c23a9233e4de13fe00532a1dcbf58ce40b445
Johnathan Bressette <bressetteprogramming@gmail.com>
2019-11-01 08:52:45 -0400

    Add deleteHeap to delete one node from the heap

--- main.c
+++ main.c
@@ -36,6 +36,75 @@ struct minHeap insertHeap(struct minHeap heap, int data)
 
 }
 
+int min(int a, int b)
+{
+    if(a < b)
+        return a;
+    else
+        return b;
+}
+
+struct minHeap deleteHeap(struct minHeap heap)
+{
+    if(heap.numElements == 0)
+    {
+        printf("There is nothing in the heap\n");
+        return heap;
+    }
+
+    else if(heap.numElements == 1)
+    {
+        heap.numElements = 0;
+        return heap;
+    }
+
+    else
+    {
+        heap.arr[0] = heap.arr[heap.numElements-1];
+        heap.numElements--;
+        int index = 0;
+
+        int leftChildIndex = (index *2) + 1;
+        int rightChildIndex = (index *2) + 2;
+
+        while(leftChildIndex < heap.numElements)
+        {
+            if(rightChildIndex == heap.numElements)
+            {
+                if(heap.arr[index] > heap.arr[leftChildIndex])
+                {
+                    int temp = heap.arr[index];
+                    heap.arr[index] = heap.arr[leftChildIndex];
+                    heap.arr[leftChildIndex] = temp;
+                }
+                break;
+            }
+
+            else
+            {
+                int min = min(heap.arr[leftChildIndex], heap.arr[rightChildIndex]);
+                if(heap.arr[index] > min)
+                {
+                    int temp = heap.arr[index];
+                    heap.arr[index] = min;
+                    if(heap.arr[leftChildIndex] == min)
+                    {
+                        heap.arr[leftChildIndex] = temp;
+                        index = leftChildIndex;
+                    }
+
+                    else
+                    {
+                        heap.arr[rightChildIndex] = temp;
+                        index = rightChildIndex;
+                    }
+                }
+            }
+        }
+    }
+
+}
+
 
 
 int main()
